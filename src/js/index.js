9308b74ec5630297018bef36422ab502f0cf79d7
Indira2406 <59651069+Indira2406@users.noreply.github.com>
2021-07-15 05:53:55 +0600

    main

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+import $ from "jquery";
+import fancybox from "@fancyapps/fancybox";
+import { Collapse } from "bootstrap";
+
+import "./adaptiveMoveBlock";
+import "./import/modules";
+import "../blocks/components/main-slider/main-slider-tabs";
+import "../blocks/components/insta-block/slider";
+import "../blocks/components/testimonials/slider";
+import "../blocks/components/cities/slider";
+
+
+var collapseElementList = [].slice.call(document.querySelectorAll(".collapse"));
+collapseElementList.map(function (collapseEl) {
+  return new Collapse(collapseEl);
+});
+
+jQuery(function ($) {
+  $(function () {
+    $("[data-fancybox]").fancybox({
+      vimeo: {
+        color: "#FFF6DB",
+        autoStart: true,
+      },
+    });
+  });
+});
